5fde3b561e00dca428a8c73d04693e964714be22
tbalint19 <toth910719balint@gmail.com>
2019-03-04 14:10:43 +0100

    component-plugin

--- src/plugin.js
+++ src/plugin.js
@@ -4,7 +4,7 @@ import GoogleLoginComponent from './google-login-component.vue'
 export default class GoogleLoginPlugin {
 
   static install(Vue, options) {
-    Vue.component('google-login-component', Object.assign(GoogleLoginPlugin, {
+    Vue.component('google-login-component', Object.assign(GoogleLoginComponent, {
       data() {
         return options;
       }
