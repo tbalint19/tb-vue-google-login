22ae753acff722821618652f6e1bf15e8b9b766d
tbalint19 <toth910719balint@gmail.com>
2019-03-04 15:28:01 +0100

    resize 2

--- src/google-login-component.vue
+++ src/google-login-component.vue
@@ -6,7 +6,7 @@
         gapi.signin2.render('my-signin2', {
           'scope': 'profile email',
           'width': 173,
-          'height': 20,
+          'height': 25,
           'longtitle': true,
           'theme': 'dark',
           'onsuccess': onSuccess,
